4dd1c82b02d3e11628752097ca96873b4dcbc333
Manuel Pichardo <mlpichardo1@gmail.com>
2018-03-31 15:27:54 +0000

    C Tutorial

---
+++ hello.c
@@ -0,0 +1,15 @@
+// require is comparable to REQUIRE
+#include<stdio.h>
+
+// int is INTEGER       below is a FUNCTION
+int main(void)
+{
+    printf("Hello World!\n");
+}
+
+
+// check50 checks your file
+// error50 helps look for error messages
+// debug50 runs a debug..breakpoints in your code > stop point/pauses
+// style50 gives advice on cleaning code
+//   ./a.out ----> will run the file
