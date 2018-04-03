56029675f24020dd6a82ff850a9eaa0e39b8a9d4
Manuel Pichardo <mlpichardo1@gmail.com>
2018-04-03 00:12:41 +0000

    number.c walkthrough with Cliff

---
+++ number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("hello there\n");
+    int integer = 5 + 8;
+    //000101 + 001000 = 001101  BINARY IS CRAZY!!!!!!
+    printf("This is my number: %i\n", integer);
+}
